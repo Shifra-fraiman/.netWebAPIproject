b2f41cfc09d7f9225915966a4d6f4ae0b5a610c4
shifra-fraiman <s0548549680@gmail.com>
2024-05-29 17:51:29 +0300

    change getAll and post by userId

--- Interfaces/ItaskListService.cs
+++ Interfaces/ItaskListService.cs
@@ -3,9 +3,9 @@ using project.Models;
 namespace project.interfaces
 {
     public interface ItaskListService{
-        List<myTask> GetAll();
+        List<myTask> GetAll(int userId);
         myTask Get(int id);
-        int Post(myTask newTask);
+        int Post(myTask newTask, int userId);
         void Put(int id, myTask newTask);
         void Delete(int id);
     }
