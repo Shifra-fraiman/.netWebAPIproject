e9a588d90f5a1cffe2796d2e16721900a5e6d1e6
shifra-fraiman <s0548549680@gmail.com>
2024-05-29 17:56:43 +0300

    & time, name of

--- middleware/MyLogMiddleware.cs
+++ middleware/MyLogMiddleware.cs
@@ -9,7 +9,7 @@ namespace project
         public MyLogMiddleware(RequestDelegate next, IWebHostEnvironment webHost)
         {
             this.next = next;
-            this.path = Path.Combine(webHost.ContentRootPath, "Data", "myLogMiddleware.txt");
+            this.path = Path.Combine(webHost.ContentRootPath, "Data", "myLog.txt");
         }
 
         public async Task Invoke(HttpContext c)
@@ -20,12 +20,13 @@ namespace project
             await next.Invoke(c);
             if (File.Exists(path))
             {
-                string createText = $"{c.Request.Path}.{c.Request.Method} took {sw.ElapsedMilliseconds}ms."
+                string createText = $"{DateTime.Now} {c.Request.Path}.{c.Request.Method} took {sw.ElapsedMilliseconds}ms."
                 + $" User: {c.User?.FindFirst("userId")?.Value ?? "unknown"}" + Environment.NewLine;
                 File.AppendAllText(path, createText);
+
             }
         }
-    }   
+    }
 }
 public static partial class MiddlewareExtensions
 {
