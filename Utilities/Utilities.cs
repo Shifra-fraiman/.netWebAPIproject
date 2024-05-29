6315b1eee760579200676623606362e88f460818
shifra-fraiman <s0548549680@gmail.com>
2024-05-29 17:41:13 +0300

    Add an extension method of ILoginService

--- Utilities/Utilities.cs
+++ Utilities/Utilities.cs
@@ -3,17 +3,24 @@ using project.Services;
 
 namespace project.Utilities
 {
-    public static class Utilities
+  public static class Utilities
+  {
+    //extention method
+    public static void AddTask(this IServiceCollection services)
     {
-        //extention method
-       public static void AddTask(this IServiceCollection services)
-       {
-         services.AddSingleton<ItaskListService, taskListFileService>();
-       }
-         //extention method
-       public static void AddUser(this IServiceCollection services)
-       {
-         services.AddSingleton<IUserService, UserService>();
-       }
+      services.AddSingleton<ItaskListService, taskListFileService>();
     }
+    //extention method
+    public static void AddUser(this IServiceCollection services)
+    {
+      services.AddSingleton<IUserService, UserService>();
+    }
+
+    //extention method
+    public static void AddLogin(this IServiceCollection services)
+    {
+      services.AddSingleton<ILoginService, LoginService>();
+    }
+
+  }
 }
